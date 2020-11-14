e5acc8fab6568e18bfed19ce86cf3c6889417483
Grant Palmer <grantpalmer@Grants-MacBook-Air.local>
2020-11-13 22:20:44 -0500

    add mcts adapter for tetris game state

---
+++ src/tetris/model/mcts.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+from common.model.mcts import TaskState
+from game import State
+
+
+@dataclass
+class TetrisTaskState(TaskState):
+    """Adapter for the tetris game state for MCTS."""
+    state: State
+
+    @property
+    def is_terminal(self):
+        return not self.actions
+    
+    @property
+    def actions(self):
+        return self.state.possible_moves
+    
+    def perform_action(self, action):
+        self.state.play_move(action)
