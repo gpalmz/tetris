7fde6e2b9e6a2fc5167f6beb0a5cb752856785de
Sarah Witt <witt.s@husky.neu.edu>
2020-11-16 10:44:48 -0500

    Display next piece

--- src/tetris/ui/game.py
+++ src/tetris/ui/game.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 import time
-from tetris.model.game import PieceType, State, create_new_state, is_block
+from tetris.model.game import PieceType, State, create_new_state, is_block, get_grid_for_piece_type
 from tetris.model.mcts import select_move
 
 
@@ -25,18 +25,30 @@ class GameBoard():
         self.square_size = 20
         self.cols = board.col_count
         self.rows = board.row_count
+        self.offset = 5
         self.height = self.cols * self.square_size
-        self.width = self.rows * self.square_size
+        self.width = (self.rows + self.offset) * self.square_size
         self.block_colors = {}
+        self.display = pygame.display.set_mode((self.height, self.width))
 
+    def fill_offset(self):
+        for i in range(4):
+            for j in range(self.cols):
+                pygame.draw.rect(self.display, (255, 255, 255), [
+                                         self.square_size * j, self.square_size * i, self.square_size, self.square_size])
+
+    def draw_square(self, color, x, y, has_border=False):
+        if has_border:
+            pygame.draw.rect(self.display, color, [self.square_size * x, self.square_size * y, self.square_size, self.square_size], 3)
+        else:
+            pygame.draw.rect(self.display, color, [self.square_size * x, self.square_size * y, self.square_size, self.square_size])
 
     def play_game(self):
         pygame.init()
-        display = pygame.display.set_mode((self.height, self.width))
         pygame.display.set_caption("Tetris")
         died = False
         state = create_new_state(self.board)
-        display.fill((220, 220, 220))
+        self.display.fill((255, 255, 255))
         pygame.display.flip()
 
         while not died:
@@ -45,14 +57,27 @@ class GameBoard():
             if move is None:
                 font = pygame.font.SysFont('Comic Sans', 20, True, False)
                 text = font.render("You lost!", True, (0, 0, 0))
-                display.blit(text, [20, 200])
+                self.display.blit(text, [20, 200])
                 pygame.display.flip()
-                break
+                died = True
 
+            cur_piece_type = state.piece_type
+            cur_grid = get_grid_for_piece_type(cur_piece_type)
             piece, state = state.play_move(move)
             placements = piece.block_placements
             piece_type = piece.piece_type
             block_color = get_color_for_type(piece_type)
+
+            self.fill_offset()
+            for space in range (0, len(cur_grid)):
+                for i, j in zip(range((len(cur_grid[space]))), range(self.cols // 2 - 1, self.cols // 2 - 1 + len(cur_grid[space]))):
+                    if is_block(cur_grid[space][i]):
+                        self.draw_square((0, 0, 0), j, (space + 1), True)
+                        self.draw_square(block_color, j, (space + 1))
+                    else:
+                        self.draw_square((255, 255, 255), j, (space + 1))
+
+                    pygame.display.flip()
 
             for placement in placements:
                 self.block_colors[placement.val] = block_color
@@ -63,17 +88,14 @@ class GameBoard():
 
                     if is_block(space):
                         cur_color = self.block_colors[space]
-
-                        pygame.draw.rect(display, (0, 0, 0), [
-                                         self.square_size * j, self.square_size * i, self.square_size, self.square_size], 3)
-                        pygame.draw.rect(display, cur_color, [
-                                         self.square_size * j, self.square_size * i, self.square_size, self.square_size])
+                        self.draw_square((0, 0, 0), j, (i + self.offset), True)
+                        self.draw_square(cur_color, j, (i + self.offset))
                     else:
-                        pygame.draw.rect(display, (255, 255, 255), [
-                                         self.square_size * j, self.square_size * i, self.square_size, self.square_size])
+                        self.draw_square((255, 255, 255), j, (i + self.offset))
                     pygame.display.flip()
 
-            time.sleep(1)
+            time.sleep(0.5)
 
         time.sleep(10)
         pygame.quit()
+
