66e11a194d794575d2cc22f034d861342421f529
Sarah Witt <witt.s@husky.neu.edu>
2020-12-06 23:30:54 -0500

    update tuning

--- src/tune.py
+++ src/tune.py
@@ -7,7 +7,7 @@ from common.util.np import NumpyJsonEncoder
 
 
 @click.command()
-@click.option("--iter-count", default=20, help="Number of iterations of random hyperparameter search; larger values will return more accurate yet slower results.")
+@click.option("--iter-count", default=20, help="Number of iterations or (sets of parameters attempted) of random hyperparameter search; larger values will return more accurate yet slower results.")
 @click.option("--sample-count", default=50, help="Number of samples tried per each hyperparameter set; larger values will return more accurate yet slower results. Must be greater than 5.")
 @click.option("--path", default="../config/params.json", help="Output file for new params in JSON format.")
 def tune(iter_count, sample_count, path):
