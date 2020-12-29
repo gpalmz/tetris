3420bb9bc094c372ef86a00311ed0fca82a656a9
Grant Palmer <grantpalmer@Grants-MacBook-Air.local>
2020-12-29 17:38:44 -0500

    constrain pieces to board when rotating in interactive player for convenience

--- src/tetris/model/gameplay/player/interactive.py
+++ src/tetris/model/gameplay/player/interactive.py
@@ -8,20 +8,43 @@ from tetris.model.gameplay.common.player import Player
 from tetris.model.strategy import select_move_random
 
 
-def _transition_move(old_move, new_move, board_state):
-    return new_move if new_move in board_state.possible_moves else old_move
+def _move_left(board_state, move):
+    new_move = Move(move.col - 1, move.orientation)
+    return new_move if new_move in board_state.possible_moves else move
 
 
-def _move_left(move, board_state):
-    return _transition_move(move, Move(move.col - 1, move.orientation), board_state)
+def _move_right(board_state, move):
+    new_move = Move(move.col + 1, move.orientation)
+    return new_move if new_move in board_state.possible_moves else move
 
 
-def _move_right(move, board_state):
-    return _transition_move(move, Move(move.col + 1, move.orientation), board_state)
+def _rotate(board_state, move):
+    new_move = Move(move.col, move.orientation.rotated_90_ccw())
+    return new_move if new_move in board_state.possible_moves else move
 
 
-def _rotate(move, board_state):
-    return _transition_move(move, Move(move.col, move.orientation.rotated_90_ccw()), board_state)
+def _constrain_right(board_state, move):
+    board_width = board_state.board.col_count
+    piece_width = board_state.get_piece_for_move(move).col_count
+    if move.col + piece_width >= board_width:
+        return Move(board_width - piece_width, move.orientation)
+    else:
+        return move
+
+
+def _rotate(board_state, move):
+    rotated_move = Move(move.col, move.orientation.rotated_90_ccw())
+    if rotated_move in board_state.possible_moves:
+        return rotated_move
+    else:
+        # sometimes rotating a piece pushes its right bound off the board.
+        # for convenience, try shifting it left. if still invalid, return original move.
+        # this only happens on the right side since pieces are anchored on the top left.
+        constrained_rotated_move = _constrain_right(board_state, rotated_move)
+        if constrained_rotated_move in board_state.possible_moves:
+            return constrained_rotated_move
+        else:
+            return move
 
 
 @dataclass
@@ -48,11 +71,11 @@ class InteractivePlayer(Player):
 
             def on_next_event(event):
                 if event.key == pygame.K_LEFT:
-                    set_move(_move_left(move, s))
+                    set_move(_move_left(s, move))
                 elif event.key == pygame.K_RIGHT:
-                    set_move(_move_right(move, s))
+                    set_move(_move_right(s, move))
                 elif event.key == pygame.K_UP:
-                    set_move(_rotate(move, s))
+                    set_move(_rotate(s, move))
                 elif event.key == pygame.K_SPACE:
                     move_subject.on_completed()
             
